de72e8fefcac4029489208799e52edf5be049006
MateusAssis <mateus.d.assis.silva@gmail.com>
2020-04-09 11:17:05 -0300

    trocaregioes working

--- exercises/trocaregioes.cpp
+++ exercises/trocaregioes.cpp
@@ -20,37 +20,31 @@ int main(int argc, char** argv){
   imshow("Original Image", image);
   waitKey();
 
-  // testing only. Remove later
-  cout<<"rows = "<< image.rows<<" ; \n cols = "<< image.cols <<endl;
-  // testing only. Remove later
-
   int dim_x = image.rows/2;
   int dim_y = image.cols/2;
 
-  cout<<"dim_x = "<<dim_x<<"; dim_y = "<< dim_y; 
+  cout<<"dim_x = "<<dim_x<<"; dim_y = "<< dim_y<<endl; 
 
+  //creating clone image which will become the processed image
+  Mat processed_image = image.clone();
+  
   Rect A_area(0, 0, dim_x, dim_y);  
-  Rect B_area(image.rows/2,0 ,dim_x, dim_y);
-  Rect C_area(0, image.cols/2, dim_x, dim_y);
-  Rect D_area(image.rows/2 , image.cols/2, dim_x, dim_y);
+  Rect B_area(dim_x,0 ,dim_x, dim_y);
+  Rect C_area(0 , dim_y, dim_x, dim_y);
+  Rect D_area(dim_x , dim_y, dim_x, dim_y);
 
   Mat A = image(A_area);
   Mat B = image(B_area);
   Mat C = image(C_area);
   Mat D = image(D_area);
 
-  A.copyTo(image(D_area));
-  B.copyTo(image(C_area));
-  C.copyTo(image(B_area));
-  D.copyTo(image(A_area));  
+  A.copyTo(processed_image(D_area));
+  B.copyTo(processed_image(C_area));
+  C.copyTo(processed_image(B_area));
+  D.copyTo(processed_image(A_area));
+  
   namedWindow("Processed Image",WINDOW_AUTOSIZE);
-  imshow("Processed Image", image);
-
-/*
-  imshow("A", A);
-  imshow("B", B);
-  imshow("C", C);
-  imshow("D", D); */
+  imshow("Processed Image", processed_image);
 
   waitKey();  
 
