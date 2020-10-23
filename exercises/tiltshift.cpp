9d02b0be2f4febda213326510c609470a08225ff
MateusAssis <mateus.d.assis.silva@gmail.com>
2020-10-23 10:31:02 -0300

    tiltshift mask working

--- exercises/tiltshift.cpp
+++ exercises/tiltshift.cpp
@@ -3,11 +3,22 @@
 #include<cmath>
 
 cv::Mat image1, image2, img_ponder;
-int x0 = 0; // center of the weighted filter image
-int band = 0; // band size
-int l1=-20,l2=30;
-float d=6;
-int altura=0;
+
+double x0 = 0; // center of the weighted filter image
+int x0_slider;
+const int x0_slider_max = 100;
+
+double band = 0; // band size
+int band_slider;
+const int band_slider_max = 100;
+
+double coef = 0;
+int coef_slider;
+const int coef_slider_max = 100;
+
+//int l1=-20,l2=30;
+//float d=6;
+//int altura=0;
 
 float alpha(float domain_point,float x0, float b,float d){
   float l1 = x0 - b;
@@ -22,10 +33,29 @@ void gerar_imagem_ponderacao(cv::Mat &imagem_ponder){
   rows = imagem_ponder.rows;
   for(int i=0; i<rows; i++){
     for(int j=0; j<cols; j++){
-      imagem_ponder.at<float>(i,j) = alpha(i,120,30.0,d);
+      imagem_ponder.at<float>(i,j) = alpha(i,x0*rows/2, band*rows/2,coef*50+1);
     }
   }
- std::cout<<"\n passei por aki \n";
+ //std::cout<<"\n passei por aki \n";
+}
+
+
+void on_x0_trackbar(int, void *){
+  x0 = (double) x0_slider/x0_slider_max;
+  gerar_imagem_ponderacao(img_ponder);
+  imshow("Tela Teste", img_ponder);
+}
+
+void on_band_trackbar(int, void *){
+  band = (double) band_slider/band_slider_max;
+  gerar_imagem_ponderacao(img_ponder);
+  imshow("Tela Teste", img_ponder);
+}
+
+void on_coef_trackbar(int, void *){
+  coef = (double) coef_slider/coef_slider_max;
+  gerar_imagem_ponderacao(img_ponder);
+  imshow("Tela Teste", img_ponder);
 }
 
 int main(){
@@ -48,8 +78,31 @@ int main(){
   std::cout<<"valor de alpha = "<<alphinha<<std::endl;
 
   img_ponder = cv::Mat(width1,height1,CV_32F);
+
+  /*
   gerar_imagem_ponderacao(img_ponder);
   cv::imshow("Imagem ponderacao",img_ponder);
-  cv::waitKey();
+  */
+  x0 = 0;
+  
+  // a partir daqui cria-se a janela
+  cv::namedWindow("Tela Teste");
+
+  char TrackbarName[50];
+  
+  std::sprintf(TrackbarName, "x0 x %d :", x0_slider_max);
+  cv::createTrackbar(TrackbarName,"Tela Teste", &x0_slider, x0_slider_max, on_x0_trackbar);
+  on_x0_trackbar(x0_slider,0);
+
+  std::sprintf(TrackbarName, "band x %d :", band_slider_max);
+  cv::createTrackbar(TrackbarName,"Tela Teste", &band_slider, band_slider_max, on_band_trackbar);
+  on_band_trackbar(band_slider,0);  
+
+  std::sprintf(TrackbarName, "coef x %d :", coef_slider_max);
+  cv::createTrackbar(TrackbarName,"Tela Teste", &coef_slider, coef_slider_max, on_coef_trackbar);
+  on_coef_trackbar(coef_slider,0); 
+
+  cv::waitKey(0);
+
   return 0;
 }
