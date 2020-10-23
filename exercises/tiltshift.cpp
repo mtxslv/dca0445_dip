3196f8c866e03a707d24f3d81a95c1e74a284297
MateusAssis <mateus.d.assis.silva@gmail.com>
2020-10-23 12:16:24 -0300

    i just only to element wise multiply

--- exercises/tiltshift.cpp
+++ exercises/tiltshift.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include<cmath>
 
-cv::Mat image1, img_ponder, img_ponder_negativo, imagem_renderizada;
+cv::Mat image1, image1_borrada, img_ponder, img_ponder_negativo, imagem_renderizada;
 
 double x0 = 0; // center of the weighted filter image
 int x0_slider;
@@ -24,6 +24,21 @@ float alpha(float domain_point,float x0, float b,float d){
   return y;
 }
 
+/*
+void montar_imagem(){
+
+  cv::Mat img_ponder_tri;
+  cv::merge(img_ponder,img_ponder,img_ponder,3,img_ponder_tri);
+
+  cv::Mat img_ponder_tri_negativo;
+  cv::merge(img_ponder_negativo,img_ponder_negativo,img_ponder_negativo,3,img_ponder_tri_negativo);
+
+  cv::Mat image_auxiliar = image1.mul(img_ponder_tri);
+  cv::Mat outra_imagem_auxiliar = image1_borrada.mul(img_ponder_tri_negativo);
+  imagem_renderizada = outra_imagem_auxiliar + image_auxiliar;
+}
+*/
+
 void gerar_imagem_ponderacao(cv::Mat &imagem_ponder){
   int rows, cols;
   cols = imagem_ponder.cols;
@@ -34,8 +49,8 @@ void gerar_imagem_ponderacao(cv::Mat &imagem_ponder){
     }
   }
   img_ponder_negativo = cv::Mat::ones(rows,cols,CV_32F)- imagem_ponder;
+  //montar_imagem();
 }
-
 
 void on_x0_trackbar(int, void *){
   x0 = (double) x0_slider/x0_slider_max;
@@ -66,6 +81,13 @@ int main(){
   std::cout <<"\t width1 = "<< width1 << "\n\t height1 = " << height1 << std::endl;
 
   img_ponder = cv::Mat(height1,width1,CV_32F);
+
+  // vamos borrar a imagem
+  cv::GaussianBlur(image1,image1_borrada,cv::Size(5,5),1.0,0.0);
+
+  //display images
+  imshow("imagem borrada", image1_borrada);
+  imshow("imagem original", image1);
 
   // a partir daqui cria-se a janela
   cv::namedWindow("Tela Teste");
