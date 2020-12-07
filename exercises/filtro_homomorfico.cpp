6c8b39dce6d142d0b6d1a24aaf59f6e5ed682032
MateusAssis <mateus.d.assis.silva@gmail.com>
2020-12-07 15:49:47 -0300

    agora o treco funciona

--- exercises/filtro_homomorfico.cpp
+++ exercises/filtro_homomorfico.cpp
@@ -32,41 +32,49 @@ void deslocaDFT(cv::Mat& image) {
 }
 
 int main(int argc, char** argv){
-  cv::Mat original_image, log_original_image, g;
-  cv::Mat_<float> original_float_image,zeros, realInput, H,tmp,tmp_exp;
-  cv::Mat padded, complexImage, magI, filter;
+  cv::Mat original_image, log_original_image, padded_image, complexImage, filter, tmp, H_pra_visualizacao;
+  cv::Mat_<float> float_padded_image, zeros, realInput, H,tmp_exp ;
   std::vector<cv::Mat> planos;
   int dft_M, dft_N;
-  float D0=1, C=1, gamma_h = 10, gamma_l=1;
+  float D0=10.0, C=0.05, gamma_h = 10.0, gamma_l=1.0;
 
-  original_image = cv::imread(argv[1],cv::IMREAD_GRAYSCALE); // obtendo a imagem
-  original_image.convertTo(original_float_image,CV_32F); // convertendo e realocando para um tipo float de 32 bits
-  cv::log(original_float_image,log_original_image); // logaritmo da imagem original
+  original_image = cv::imread(argv[1],cv::IMREAD_GRAYSCALE); 
+  if(original_image.empty())
+    return -1;
 
-  dft_M = cv::getOptimalDFTSize(log_original_image.rows);
-  dft_N = cv::getOptimalDFTSize(log_original_image.cols);
+  cv::imshow("original_image", original_image);
+  cv::waitKey();
 
-  std::cout<<"\ndft_M = "<<dft_M<<"\ndft_N = "<<dft_N<<std::endl;
+  // ajeitar o tamanho da imagem original
+    // identifica os tamanhos otimos para
+    // calculo do FFT
+  dft_M = cv::getOptimalDFTSize(original_image.rows);
+  dft_N = cv::getOptimalDFTSize(original_image.cols);
 
-  cv::copyMakeBorder(log_original_image, padded, 0,
-               dft_M - log_original_image.rows, 0,
-               dft_N - log_original_image.cols,
-               cv::BORDER_CONSTANT, cv::Scalar::all(0));
-               
+    // realiza o padding da imagem
+  cv::copyMakeBorder(original_image, padded_image, 0, dft_M - original_image.rows, 0,
+                     dft_N - original_image.cols, cv::BORDER_CONSTANT,
+                     cv::Scalar::all(1));
+
+  cv::imshow("padded_image", padded_image);
+  cv::waitKey();
+
+  // tirar o log
+  padded_image.convertTo(float_padded_image,CV_32F); // se não converter, o log não funciona
+  log_original_image = cv::Mat(padded_image.size(),CV_32F);
+  cv::log(float_padded_image,log_original_image);
+
   // parte imaginaria da matriz complexa (preenchida com zeros)
-  zeros = cv::Mat_<float>::zeros(padded.size());
-  
-  // limpa o array de matrizes que vao compor a
-  // imagem complexa
-  planos.clear();
+  zeros = cv::Mat_<float>::zeros(float_padded_image.size());
+
+  // prepara a matriz complexa para ser preenchida
+  complexImage = cv::Mat(float_padded_image.size(), CV_32FC2, cv::Scalar(0));
+
   // cria a compoente real
-  realInput = cv::Mat_<float>(padded);
+  realInput = cv::Mat_<float>(float_padded_image);
   // insere as duas componentes no array de matrizes
   planos.push_back(realInput);
   planos.push_back(zeros);
-
-  // prepara a matriz complexa para ser preenchida
-  complexImage = cv::Mat(padded.size(), CV_32FC2, cv::Scalar(0));
 
   // combina o array de matrizes em uma unica
   // componente complexa
@@ -74,17 +82,22 @@ int main(int argc, char** argv){
 
   // calcula o dft
   cv::dft(complexImage, complexImage);
-
   // realiza a troca de quadrantes
   deslocaDFT(complexImage);
 
+  // NO ARQUIVO dft.cpp, DA LINHA 142 A 164 É SÓ SOBRE FILTRO
+
+  // a função de transferencia (filtro de frequencia) deve ter o
+  // mesmo tamanho e tipo da matriz complexa
+  filter = complexImage.clone();
 
   // cria uma matriz temporária para criar as componentes real
   // e imaginaria do filtro ideal
   tmp = cv::Mat(dft_M, dft_N, CV_32F);
-  H = cv::Mat(dft_M,dft_N,CV_32F);
+  H = cv::Mat_<float>(dft_M,dft_N,CV_32F);
+  filter = cv::Mat_<float>(dft_M,dft_N,CV_32F);
 
-  // prepara um pedaço de filtro: -C(D²(u,v)/D0²)
+    // prepara um pedaço de filtro: -C(D²(u,v)/D0²)
   for (int i = 0; i < dft_M; i++) {
     for (int j = 0; j < dft_N; j++) {
       tmp.at<float>(i, j) = -C*((i - dft_M / 2) * (i - dft_M / 2) + (j - dft_N / 2) * (j - dft_N / 2))/(D0*D0);
@@ -96,9 +109,12 @@ int main(int argc, char** argv){
 
   // cria a matriz com as componentes do filtro e junta
   // ambas em uma matriz multicanal complexa
-  cv::Mat comps[]= {H, H};
-  filter = complexImage.clone();
+  cv::Mat comps[] = {H, H};
   cv::merge(comps, 2, filter);
+
+  cv::normalize(H, H_pra_visualizacao, 0, 255, cv::NORM_MINMAX,CV_8UC1);
+  cv::imshow("H",H_pra_visualizacao);
+  cv::waitKey();
 
   // aplica o filtro de frequencia
   cv::mulSpectrums(complexImage, filter, complexImage, 0);
@@ -116,43 +132,10 @@ int main(int argc, char** argv){
   // imagem filtrada
   cv::split(complexImage, planos);
 
-
-
   // normaliza a parte real para exibicao
   cv::normalize(planos[0], planos[0], 0, 1, cv::NORM_MINMAX);
   cv::imshow("filtrada", planos[0]);  
-
-
-  /*
-  // não sei se mostro
-  planos.clear();
-  cv::split(complexImage, planos);
-
-  cv::Mat magn, angl, anglInt, magnInt;
-  cv::cartToPolar(planos[0], planos[1], magn, angl, false);
-  cv::normalize(angl, angl, 0, 255, cv::NORM_MINMAX);
-  angl.convertTo(anglInt, CV_8U);
-  cv::imshow("Angulo de Fase", anglInt);
-
-  cv::magnitude(planos[0], planos[1], planos[0]);
-  magI = planos[0];
-  magI += cv::Scalar::all(1);
-  cv::log(magI, magI);
-  cv::normalize(magI, magI, 0, 255, cv::NORM_MINMAX);
-  magI.convertTo(magnInt, CV_8U);
-  cv::imshow("Espectro", magnInt);
-  */
-
- // cv::imshow("image:",original_image); //exibição
- // cv::imshow("log image:",log_original_image); //exibição
   cv::waitKey();
+  
   return 0; 
 }
-
-
-/*
-Mat_<float> fm;
-
-m.convertTo(fm,CV_32F);
-
-*/
